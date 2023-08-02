a549f0534e9267d5f6b7f01c2c032be58a3491c1
VaibDeveloper <135333002+VaibDeveloper@users.noreply.github.com>
2023-08-02 14:35:26 +0530

    UpdateD Questions.js

--- components/Questions.js
+++ components/Questions.js
@@ -1,31 +1,191 @@
-//import liraries
-
 const questions = [
-  {
-    questionImage: require("../assets/images/sea.png"),
-    Options: ["SEE", "SEA", "CEE", "SAE"],
-    correctOption: "SEA",
-  },
-  {
-    questionImage: require("../assets/images/male.png"),
-    Options: ["MAIL", "MAEL", "MALE", "MALL"],
-    correctOption: "MALE",
-  },
-  {
-    questionImage: require("../assets/images/write.png"),
-    Options: ["RIGHT", "WRIGHT", "RITE", "WRITE"],
-    correctOption: "WRITE",
-  },
-  {
-    questionImage: require("../assets/images/deer.png"),
-    Options: ["DEAR", "DARE", "DEER", "DIER"],
-    correctOption: "DEER",
-  },
-  {
-    questionImage: require("../assets/images/nose.jpg"),
-    Options: ["NOSE", "KNOWS", "KNOSE", "NOZE"],
-    correctOption: "NOSE",
-  },
+  [
+    {
+      questionImage: require("../assets/images/one.png"),
+      Options: ["ONE", "WON", "VUN", "WONE"],
+      correctOption: "ONE",
+    },
+    {
+      questionImage: require("../assets/images/sun.png"),
+      Options: ["SONE", "SUN", "SON", "SAN"],
+      correctOption: "SUN",
+    },
+    {
+      questionImage: require("../assets/images/ant.png"),
+      Options: ["ANTE", "ENT", "ANT", "AUNT"],
+      correctOption: "ANT",
+    },
+    {
+      questionImage: require("../assets/images/deer.png"),
+      Options: ["DEAR", "DARE", "DEER", "DIER"],
+      correctOption: "DEER",
+    },
+    {
+      questionImage: require("../assets/images/four.png"),
+      Options: ["FORR", "FOUR", "FAR", "FOR"],
+      correctOption: "FOUR",
+    },
+  ],
+  [
+    {
+      questionImage: require("../assets/images/two.png"),
+      Options: ["TEW", "TWO", "TUU", "TOO"],
+      correctOption: "TWO",
+    },
+    {
+      questionImage: require("../assets/images/bear.png"),
+      Options: ["BARE", "BIER", "BEER", "BEAR"],
+      correctOption: "BEAR",
+    },
+    {
+      questionImage: require("../assets/images/nose.jpg"),
+      Options: ["NOSE", "NOZE", "KNOSE", "KNOWS"],
+      correctOption: "NOSE",
+    },
+    {
+      questionImage: require("../assets/images/pear.png"),
+      Options: ["PAIR", "PEAR", "PARE", "PEER"],
+      correctOption: "PEAR",
+    },
+    {
+      questionImage: require("../assets/images/ring.png"),
+      Options: ["RIGG", "RENG", "WRING", "RING"],
+      correctOption: "RING",
+    },
+  ],
+  [
+    {
+      questionImage: require("../assets/images/sea.png"),
+      Options: ["SEE", "SEA", "CEE", "SAE"],
+      correctOption: "SEA",
+    },
+    {
+      questionImage: require("../assets/images/plane.png"),
+      Options: ["PLANE", "PLAN", "PLEN", "PLAIN"],
+      correctOption: "PLANE",
+    },
+    {
+      questionImage: require("../assets/images/rose.png"),
+      Options: ["ROSS", "ROZE", "ROSE", "ROWS"],
+      correctOption: "ROSE",
+    },
+    {
+      questionImage: require("../assets/images/hair.png"),
+      Options: ["HIER", "HAIR", "HARE", "HEIR"],
+      correctOption: "HAIR",
+    },
+    {
+      questionImage: require("../assets/images/flower.png"),
+      Options: ["FLOUER", "FLOWER", "FLOWR", "FLOUR"],
+      correctOption: "FLOWER",
+    },
+  ],
+  [
+    {
+      questionImage: require("../assets/images/feet.png"),
+      Options: ["FIT", "FEET", "FEAT", "FIIT"],
+      correctOption: "FEET",
+    },
+    {
+      questionImage: require("../assets/images/ball.png"),
+      Options: ["BAAL", "BOLE", "BOWL", "BALL"],
+      correctOption: "BALL",
+    },
+    {
+      questionImage: require("../assets/images/fairy.png"),
+      Options: ["FARRY", "FAERY", "FAIRY", "FERRY"],
+      correctOption: "FAIRY",
+    },
+    {
+      questionImage: require("../assets/images/freeze.png"),
+      Options: ["FRIIZE", "FREEZ", "FREEZE", "FREES"],
+      correctOption: "FREEZE",
+    },
+    {
+      questionImage: require("../assets/images/stair.png"),
+      Options: ["STEER", "STARE", "STERE", "STAIR"],
+      correctOption: "STAIR",
+    },
+  ],
+  [
+    {
+      questionImage: require("../assets/images/band.png"),
+      Options: ["BENNED", "BAND", "BEND", "BANNED"],
+      correctOption: "BAND",
+    },
+    {
+      questionImage: require("../assets/images/pray.png"),
+      Options: ["PREY", "PREA", "PRAY", "PREE"],
+      correctOption: "PRAY",
+    },
+    {
+      questionImage: require("../assets/images/cell.png"),
+      Options: ["CELL", "SELL", "CALE", "SALL"],
+      correctOption: "CELL",
+    },
+    {
+      questionImage: require("../assets/images/knot.png"),
+      Options: ["KNOWT", "NOTT", "KNOT", "NOT"],
+      correctOption: "KNOT",
+    },
+    {
+      questionImage: require("../assets/images/cereal.png"),
+      Options: ["SERIAL", "SEREAL", "CIRIAL", "CEREAL"],
+      correctOption: "CEREAL",
+    },
+  ],
+  [
+    {
+      questionImage: require("../assets/images/waste.png"),
+      Options: ["WAEST", "WAIST", "VASTE", "WASTE"],
+      correctOption: "WASTE",
+    },
+    {
+      questionImage: require("../assets/images/hole.png"),
+      Options: ["HALL", "HOLE", "HOLL", "WHOLE"],
+      correctOption: "HOLE",
+    },
+    {
+      questionImage: require("../assets/images/write.png"),
+      Options: ["RIGHT", "WRIGHT", "RITE", "WRITE"],
+      correctOption: "WRITE",
+    },
+    {
+      questionImage: require("../assets/images/root.jpg"),
+      Options: ["RUTE", "ROOTE", "ROUTE", "ROOT"],
+      correctOption: "ROOT",
+    },
+    {
+      questionImage: require("../assets/images/sail.png"),
+      Options: ["SAIL", "SEEL", "SELL", "SALE"],
+      correctOption: "SAIL",
+    },
+  ],
+  [
+    {
+      questionImage: require("../assets/images/knight.png"),
+      Options: ["KNITE", "NITE", "NIGHT", "KNIGHT"],
+      correctOption: "KNIGHT",
+    },
+    {
+      questionImage: require("../assets/images/male.png"),
+      Options: ["MAIL", "MAEL", "MALE", "MALL"],
+      correctOption: "MALE",
+    },
+    {
+      questionImage: require("../assets/images/meat.png"),
+      Options: ["MIET", "MATE", "MEAT", "MEET"],
+      correctOption: "MEAT",
+    },
+    {
+      questionImage: require("../assets/images/maize.png"),
+      Options: ["MAIZE", "MAEZ", "MAZE", "MEIZ"],
+      correctOption: "MAIZE",
+    },
+    {
+      questionImage: require("../assets/images/tail.png"),
+      Options: ["TAEL", "TAIL", "TALL", "TALE"],
+      correctOption: "TAIL",
+    },
+  ],
 ];
-
-export default questions;
